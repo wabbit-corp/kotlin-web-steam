be3197aaa39000aeb715d83c3905e0281014cb12
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2025-01-12 18:27:17 -0500

    Refactor build scripts: Adjust indentation and remove unused repository. No changes affecting binary compatibility.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ rootProject.name = "kotlin-web-steam"
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
-        maven("https://papermc.io/repo/repository/maven-public/")
+        
     }
 }
 
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
+        
     }
 }
